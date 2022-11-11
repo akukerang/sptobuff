bc404d12ef9da0f0e43cf93cdf5051e4104d5f23
aku <gabrielsuoth@gmail.com>
2022-11-10 22:47:04 -0500

    switched bs4 parser to lxml
    can't pip install lxml with python 3.11, so downgrade to 3.10.7

--- priceEmpireBuff.py
+++ priceEmpireBuff.py
@@ -15,7 +15,7 @@ def getBuffPrice(itemname):
     driver.get(URL)
     time.sleep(2.5)
     listings = driver.page_source
-    soup = BeautifulSoup(listings, "html5lib")
+    soup = BeautifulSoup(listings, "xml")
     # Gets the item link
     item = soup.find(title=str(itemname))
     itemhref = item.get('href')
@@ -24,7 +24,7 @@ def getBuffPrice(itemname):
     driver.get(URL2)
     time.sleep(2.5)
     itempage = driver.page_source
-    soup2 = BeautifulSoup(itempage, "html5lib")
+    soup2 = BeautifulSoup(itempage, "xml")
     buffListing = soup2.find('a', href=re.compile("buff163"))
     buffPrice = buffListing.find('div', class_="price color-price").getText()
     buffPrice = buffPrice.replace('$', '')
