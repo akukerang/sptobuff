ff4447bad68c3a972d66cd3078b0bc5796ab2700
aku <gabrielsuoth@gmail.com>
2022-11-11 19:13:43 -0500

    redid api call on skinport, returns 30 day avg now
    improved runtime

--- classes/skinport.py
+++ classes/skinport.py
@@ -1,13 +1,13 @@
 import requests
 
 def getSkinportPrice(itemname):
-    r = requests.get("https://api.skinport.com/v1/items", params={
+    params = {
+        "market_hash_name": str(itemname),
         "app_id": 730,
-        "currency": "USD",
-        "tradable": 1
-    }).json()
-    outputRequest = [x for x in r if x['market_hash_name'] == itemname]
-    for i in outputRequest:
-        return i['min_price']
+        "currency": "USD"
+    }
+    r = requests.get("https://api.skinport.com/v1/sales/history", params = params).json()
+    return float(r[0]['last_30_days']['avg'])
+
 
 
