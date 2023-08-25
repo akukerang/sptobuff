b347c49cec96d9d8ce7d36c4778575752ce54845
Tasdv <91825564+Tasdv@users.noreply.github.com>
2023-08-25 11:53:41 +0800

    Added support for return of no listing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from classes.buff import Buff
 from flask import Flask, render_template, request
 import pandas as pd
 import json
-
+import traceback
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -24,15 +24,20 @@ def mainpage():
         skin = str(request.form.get('skinName'))
         if(skin): #if input not empty run
             try:
+                
                 sp = getSkinportPrice(skin,s_csrf,s__cf_bm)
-                print(f'{sp} SP')
-                buff = b.getBuffPrice(skin)
-                print(f'{buff} buff')
-                afterFees = round((buff - (buff * 0.025)),2) #after fees Buff
-                profit = round(afterFees - sp,2)
-                gain = round((profit / sp) * 100,6)
-                results = [skin, '$'+str(sp), '$'+str(buff), '$'+str(afterFees), '$'+str(profit), str(gain)+'%']
+                if isinstance(sp, str):
+                    error = "No listing for that skin found on Skinport"
+                else:
+                    print(f'{sp} SP')
+                    buff = b.getBuffPrice(skin)
+                    print(f'{buff} buff')
+                    afterFees = round((buff - (buff * 0.025)),2) #after fees Buff
+                    profit = round(afterFees - sp,2)
+                    gain = round((profit / sp) * 100,6)
+                    results = [skin, '$'+str(sp), '$'+str(buff), '$'+str(afterFees), '$'+str(profit), str(gain)+'%']
             except:
+                print(traceback.format_exc())
                 error = "Please try another skin. Value not found"
         else:
             error = "Please enter a value"
