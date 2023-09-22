a8cfa0247583e2243724836bd03d53d560fb7266
aku <gabrielsuoth@gmail.com>
2023-09-22 18:13:49 -0400

    Change skinport functionality, cookie stuff.

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
-from tkinter import ttk, Frame, Entry, Listbox
+from tkinter import Frame, Entry, Listbox
 import csv
+import json
+from classes.skinport import skinport
+from classes.buff import Buff
 
 skin_names = []
 
@@ -9,6 +12,15 @@ with open("csv/skins.csv", "r", encoding="utf-8") as f:
     for row in reader:
         skin_names.append(row[0])
 skin_names = skin_names[1:]
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+b = Buff(config["Cookies"])
+s = skinport()
+
+
+
 def checkKey(event):
     value = event.widget.get()
     if value == '':
@@ -23,13 +35,18 @@ def checkKey(event):
             if value.lower() in item.lower():
                 data.append(item)                
     update(data)
-   
-   
+
+
 def update(data):
     skinList.delete(0, 'end')
     for item in data:
         skinList.insert('end', item)
 
+def displayError(text):
+    submitButton.pack_forget()
+    errorMessage.config(text=text)
+    errorMessage.pack(pady=10)
+    submitButton.pack(pady=30)
 
 def Submit():
     try:
@@ -37,13 +54,37 @@ def Submit():
         selectedItem = skinList.get(selectedIndex[0])
         print("Selected item: " + selectedItem)
         errorMessage.pack_forget()
+        try:
+            print('here0')
+            sp = s.getSkinportPrice(selectedItem)
+            print('here1')
+            if isinstance(sp, str):
+                displayError("No listing for that skin found on Skinport")
+            print(f'{sp} SP')
+            buff = b.getBuffPrice(selectedItem)
+            print(f'{buff} buff')
+            afterFees = round((buff - (buff * 0.025)),2) #after fees Buff
+            profit = round(afterFees - sp,2)
+            gain = round((profit / sp) * 100,6)
+            results = [selectedItem, '$'+str(sp), '$'+str(buff), '$'+str(afterFees), '$'+str(profit), str(gain)+'%']
+            updateResults(results)
+            print(results)
+        except:
+            displayError("Please try another skin. Value not found")
+                
     except:
-        submitButton.pack_forget()
-        errorMessage.pack(pady=10)
-        submitButton.pack(pady=30)
+        displayError("Please enter a skin")
+
+
+def updateResults(results):
+    pass
+
+
 
 app = tk.Tk()
 app.title("spToBuff")
+app.resizable(False, False)
+
 
 selectFrame = Frame(app, width=300, height=400, bg="lightgray")
 selectFrame.pack(side="left", fill="both", expand=True)
@@ -58,15 +99,19 @@ skinEntry = Entry(selectFrame, width=50)
 skinEntry.bind("<KeyRelease>", checkKey)
 skinEntry.pack()
 
-
 skinList = Listbox(selectFrame, width=50)
 update(skin_names)
 
-errorMessage = tk.Label(selectFrame, text="Please select a skin.", fg="red")
+errorMessage = tk.Label(selectFrame, fg="red")
 
 submitButton = tk.Button(selectFrame, text="Submit", command=Submit)
 submitButton.pack(pady=30)
 
-app.resizable(False, False)
+
+
+
+
+
+
 
 app.mainloop()
